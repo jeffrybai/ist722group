3cd3e102623388521c045397a982ceae536d8897
Maybel Herrera <maherrer@g.syr.edu>
2024-12-05 05:26:46 +0000

    final draft on dim tables

--- pubs/models/dim_titles.sql
+++ pubs/models/dim_titles.sql
@@ -2,6 +2,12 @@ with stg_titles as (
     select * from {{ source('pubs','Titles')}}
 )
 select  
-    {{ dbt_utils.generate_surrogate_key(['stg_titles.title_id']) }} as title_key,
-    stg_titles.*
+    {{ dbt_utils.generate_surrogate_key(['stg_titles.title_id']) }} as titles_key,
+    title_id,
+    title,
+    type as title_category,
+    pub_id,
+    price as title_price,
+    royalty as title_royalty,
+    ytd_sales as title_ytd_sales
 from stg_titles
