ea1d55411b6e80b7678627af5b8f62af160959af
Maybel Herrera <maherrer@g.syr.edu>
2024-12-07 04:20:45 +0000

    fact_royalty_disbursment sql corrected code

--- pubs/models/fact_royalty_disbursement.sql
+++ pubs/models/fact_royalty_disbursement.sql
@@ -1,48 +1,44 @@
-WITH stg_titles AS (
-    SELECT
-        t.titles_key,
-        t.publisher_id AS publishers_key,
-        t.published_date,
-        SUM(t.title_ytd_sales) AS ytd_sales,
-        SUM(t.title_ytd_sales * (t.title_royalty / 100)) AS royalty_amount_per_title
-    FROM {{ ref('dim_titles') }} t
-    GROUP BY t.titles_key, t.publisher_id, t.published_date
+WITH
+dim_titles AS (
+    SELECT title_id, titles_key, title_ytd_sales, title_royalty , title
+    FROM {{ ref('dim_titles') }}
 ),
-stg_titleauthor AS (
-    SELECT
-        ta.title_id AS titles_key,
-        CONCAT(ta.title_id, '_', ta.au_id) AS author_title_key, 
-        ta.royaltyper AS author_royalty_rate
-    FROM {{ source('pubs', 'TitleAuthor') }} ta
+dim_authors AS (
+    SELECT author_id, authors_key,author_name
+    FROM {{ ref('dim_authors') }}
 ),
-stg_royalties_by_author AS (
-    SELECT
-        t.titles_key,
-        ta.author_title_key,
-        SUM(t.title_ytd_sales) AS ytd_sales,
-        SUM(t.title_ytd_sales * (ta.author_royalty_rate / 100)) AS royalty_amount_per_author
-    FROM {{ ref('dim_titles') }} t
-    INNER JOIN stg_titleauthor ta
-        ON t.titles_key = ta.titles_key
-    GROUP BY t.titles_key, ta.author_title_key
+dim_publishers AS (
+    SELECT publisher_id, publishers_key 
+    FROM {{ ref('dim_publishers') }}
 ),
-stg_dates AS (
-    SELECT
-        datekey,
-        date
+dim_date AS (
+    SELECT 
+        DATEKEY,
+        DATE,
+        YEAR,
+        MONTH,
+        QUARTER,
+        DAY, 
+        DAYOFWEEK,
+        WEEKOFYEAR,
+        DAYOFYEAR,
+        QUARTERNAME,
+        MONTHNAME,
+        DAYNAME,
+        WEEKDAY
     FROM {{ ref('dim_date') }}
 )
-SELECT
-    rt.titles_key,
-    ra.author_title_key,
-    d.datekey as date_key,
-    rt.publishers_key,
-    rt.published_date,
-    rt.ytd_sales,
-    rt.royalty_amount_per_title,
-    ra.royalty_amount_per_author
-FROM stg_titles rt
-LEFT JOIN stg_royalties_by_author ra
-    ON rt.titles_key = ra.titles_key
-INNER JOIN stg_dates d
-    ON d.date = rt.published_date
+
+SELECT 
+    t.title_id,
+    t.au_id,
+    a.author_name,
+    dt.title,
+    dt.title_ytd_sales,
+    dt.title_royalty,
+    (dt.title_ytd_sales * dt.title_royalty / 100) AS royalty_amount_per_title,
+    t.royaltyper,
+    (dt.title_ytd_sales * t.royaltyper / 100) AS royalty_amount_per_author
+FROM RAW.PUBS.TitleAuthor t
+LEFT JOIN dim_titles dt ON t.title_id = dt.title_id
+LEFT JOIN dim_authors a ON t.au_id = a.author_id
