97b0a40d69e45b01b86adb8ca2508e67fe1190f4
Maybel Herrera <maherrer@g.syr.edu>
2024-12-07 06:49:21 +0000

    last attempt for the night

--- pubs/models/fact_royalty_disbursement.sql
+++ pubs/models/fact_royalty_disbursement.sql
@@ -1,51 +1,45 @@
-WITH
-dim_titles AS (
-    SELECT title_id, titles_key, title_ytd_sales, title_royalty , title, published_date, publisher_id
+WITH dim_titles AS (
+    SELECT 
+        title_id, 
+        titles_key, 
+        title_ytd_sales, 
+        title_royalty, 
+        title, 
+        published_date, 
+        publisher_id
     FROM {{ ref('dim_titles') }}
 ),
 dim_authors AS (
-    SELECT author_id, authors_key,author_name
+    SELECT 
+        author_id, 
+        authors_key, 
+        author_name
     FROM {{ ref('dim_authors') }}
 ),
 dim_publishers AS (
-    SELECT publisher_id,publishers_key,publisher_name ,
-    FROM {{ ref('dim_publishers') }}
-),
-dim_date AS (
     SELECT 
-        DATEKEY,
-        DATE,
-        YEAR,
-        MONTH,
-        QUARTER,
-        DAY, 
-        DAYOFWEEK,
-        WEEKOFYEAR,
-        DAYOFYEAR,
-        QUARTERNAME,
-        MONTHNAME,
-        DAYNAME,
-        WEEKDAY
-    FROM {{ ref('dim_date') }}
+        publisher_id, 
+        publishers_key, 
+        publisher_name
+    FROM {{ ref('dim_publishers') }}
 )
-
 SELECT 
-   dt.titles_key,
-    t.title_id,
+    dt.titles_key,
+    dt.title_id,
     a.authors_key,
     a.author_id,
     p.publishers_key,
-    dt.publisher_id,
+    p.publisher_id,
+    dt.title,
     p.publisher_name,
     dt.published_date,
     a.author_name,
-    dt.title,
     dt.title_ytd_sales,
     dt.title_royalty,
     (dt.title_ytd_sales * dt.title_royalty / 100) AS royalty_amount_per_title,
     t.royaltyper,
-    (dt.title_ytd_sales * t.royaltyper / 100) AS royalty_amount_per_author,
+    (dt.title_ytd_sales * t.royaltyper / 100) AS royalty_amount_per_author
 FROM RAW.PUBS.TitleAuthor t
 LEFT JOIN dim_titles dt ON t.title_id = dt.title_id
 LEFT JOIN dim_authors a ON t.au_id = a.author_id
-LEFT JOIN dim_publishers p on dt.publisher_id= p.publisher_id
+LEFT JOIN dim_publishers p ON dt.publisher_id = p.publisher_id
