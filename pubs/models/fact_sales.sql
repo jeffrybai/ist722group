b1c912e6d61d382a6cc436d4f4fb0e6981c46429
Victoria Zangen <vezangen@g.syr.edu>
2024-12-07 18:58:48 +0000

    completed, fact_sales.yml and fact_sales.sql

--- pubs/models/fact_sales.sql
+++ pubs/models/fact_sales.sql
@@ -2,10 +2,11 @@ with stg_titles as (
     select 
         {{dbt_utils.generate_surrogate_key(['title_id']) }} as titles_key,
         title_id,
-        title as title_title,
+        title,
         pub_id,
-        price as title_price,
-        ytd_sales as title_ytd_sales
+        price,
+        ytd_sales,
+        royalty
     from {{source('pubs','Titles') }}
 ),
 stg_publishers as (
@@ -24,32 +25,30 @@ stg_stores as (
 ),
 stg_sales as (
     select
+        {{dbt_utils.generate_surrogate_key(['ord_num','title_id','stor_id']) }} as order_number,
         ord_num,
-        {{dbt_utils.generate_surrogate_key(['stor_id']) }} as stores_key,
         title_id,
-        ord_date as order_date,
-        qty as quantity
+        stor_id,
+        ord_date,
+        qty
     from {{source ('pubs', 'Sales') }}
-    group by ord_num, ord_date, title_id, stor_id,qty
-),
-
+)
 select
-    s.ord_num,
+    s.order_number,
     t.titles_key,
     p.publishers_key,
-    s.stores_key,
-    s.order_date
-    quantity,
-    title_title,
-    title_price,
-    title_ytd_sales,
-    (s.quantity * t.title_price) as extended_price_amount,
-    (s.quantity * t.title_price * d.discount) as discount_amount,
-    ((s.quantity * t.title_price) - (s.quantity * t.title_price * d.discount)) as net_sales_amount,
-    ((s.quantity * t.title_price) * ta.royaltyper) as total_royalty_amount
-from raw.pubs.sales s
-join raw.pubs.titles t on s.title_id = t.title_id
-join raw.pubs.publishers p on t.pub_id  = p.pub_id
-join raw.pubs.stores st on s.stores_key= st.stores_key
-left join {{ source('pubs','Discounts') }} d on s.stores_key = d.stor_id
-left join {{ source('pubs','TitleAuthor') }} ta on t.title_id = ta.title_id
+    st.stores_key,
+    s.ord_date as order_date,
+    s.qty as quantity,
+    t.title as title_title,
+    t.price as title_price,
+    t.ytd_sales as title_ytd_sales,
+    (s.qty * t.price) as extended_price_amount,
+    (s.qty * t.price * d.discount) as discount_amount,
+    ((s.qty * t.price) - (s.qty * t.price * d.discount)) as net_sales_amount,
+    ((s.qty * t.price) * t.royalty) as total_royalty_amount,
+from stg_sales as s 
+join stg_titles as t on s.title_id = t.title_id
+join stg_publishers as p on t.pub_id  = p.pub_id
+join stg_stores as st on s.stor_id= st.stor_id
+left join {{ source('pubs','Discounts') }} as d on s.stor_id = d.stor_id
