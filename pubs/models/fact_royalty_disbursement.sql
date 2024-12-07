f16f0a11c9b02ac2507d9b74e9c1cc5c340d1247
Maybel Herrera <maherrer@g.syr.edu>
2024-12-07 17:19:17 +0000

    attempted stg didnt work attempted to add date dimension did not wor; just got a lot of null values under date month year. just change the select statement to read select* instead od specific columns ..this is better

--- pubs/models/fact_royalty_disbursement.sql
+++ pubs/models/fact_royalty_disbursement.sql
@@ -1,26 +1,16 @@
 WITH dim_titles AS (
-    SELECT 
-        title_id, 
-        titles_key, 
-        title_ytd_sales, 
-        title_royalty, 
-        title, 
-        published_date, 
-        publisher_id
+    SELECT *
+
     FROM {{ ref('dim_titles') }}
 ),
 dim_authors AS (
-    SELECT 
-        author_id, 
-        authors_key, 
-        author_name
+    SELECT *
+
     FROM {{ ref('dim_authors') }}
 ),
 dim_publishers AS (
-    SELECT 
-        publisher_id, 
-        publishers_key, 
-        publisher_name
+    SELECT *
+
     FROM {{ ref('dim_publishers') }}
 )
 SELECT 
@@ -38,7 +28,7 @@ SELECT
     dt.title_royalty,
     (dt.title_ytd_sales * dt.title_royalty / 100) AS royalty_amount_per_title,
     t.royaltyper,
-    (dt.title_ytd_sales * t.royaltyper / 100) AS royalty_amount_per_author
+    (dt.title_ytd_sales * t.royaltyper / 100) AS royalty_amount_per_author,
 FROM RAW.PUBS.TitleAuthor t
 LEFT JOIN dim_titles dt ON t.title_id = dt.title_id
 LEFT JOIN dim_authors a ON t.au_id = a.author_id
