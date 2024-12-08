c20b82919d71b875da0ef59d8d64cf0b4b6388ce
Jeffry Bai <jbai06@g.syr.edu>
2024-12-08 19:36:47 +0000

    update employee to include dim_date

--- pubs/models/fact_employees.sql
+++ pubs/models/fact_employees.sql
@@ -3,6 +3,6 @@ select
     {{ dbt_utils.generate_surrogate_key(['e.emp_id']) }} as employee_key,
     {{ dbt_utils.generate_surrogate_key(['e.pub_id']) }} as publisher_key,
     {{ dbt_utils.generate_surrogate_key(['e.job_id']) }} as job_key,
-    e.hire_date
+    to_char(e.HIRE_DATE,'YYYYMMDD') as HIRE_DATE_KEY
 from
     {{ source('pubs', 'Employee') }} e
--- pubs/models/obt_employees.sql
+++ pubs/models/obt_employees.sql
@@ -11,6 +11,10 @@ stg_jobs as (
 
 stg_publishers as (
     select * from {{ ref('dim_publishers') }}
+),
+
+stg_date AS (
+    SELECT * FROM {{ ref('dim_date') }}
 )
 
 select
@@ -24,15 +28,17 @@ select
         else 
             concat(employee_firstname, ' ', employee_middle_initial, ' ', employee_lastname)
     end as employee_fullname,
-    e.hire_date,
     j.job_desc,
     p.publisher_name as employee_company_name,
     p.publisher_city as employee_city,
     p.publisher_state as employee_state,
-    p.publisher_country as employee_country
+    p.publisher_country as employee_country,
+    d.date as EMPLOYEE_HIRE_DATE
 from
         stg_employees e 
     left join stg_jobs j 
         on e.job_key = j.job_key
     left join stg_publishers p 
         on e.publisher_key = p.publishers_key
+    left join stg_date d 
+        on e.HIRE_DATE_KEY = d.date_key
