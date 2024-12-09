7d3f310ed67baf2e7a05ef04fc638e5ff506baa6
Maybel Herrera <maherrer@g.syr.edu>
2024-12-09 08:00:25 +0000

    change royalty additive facts . no longer using ytd, updated obt table

--- pubs/models/fact_royalty_disbursement.sql
+++ pubs/models/fact_royalty_disbursement.sql
@@ -1,50 +1,56 @@
 WITH dim_titles AS (
     SELECT *
-
     FROM {{ ref('dim_titles') }}
 ),
 dim_authors AS (
     SELECT *
-
     FROM {{ ref('dim_authors') }}
 ),
 dim_publishers AS (
     SELECT *
-
     FROM {{ ref('dim_publishers') }}
 ),
 dim_date AS (
     SELECT *
-
     FROM {{ ref('dim_date') }}
 ),
-
-stg_TitleAuthors as (select
-    {{ dbt_utils.generate_surrogate_key(['au_id']) }} as authors_key, 
-    {{ dbt_utils.generate_surrogate_key(['title_id']) }} as titles_key, 
-    AU_ORD, 
-    royaltyper ,                                             
-FROM {{ source('pubs', 'TitleAuthor')}}
+stg_TitleAuthors AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['au_id']) }} AS authors_key, 
+        {{ dbt_utils.generate_surrogate_key(['title_id']) }} AS titles_key, 
+        AU_ORD, 
+        royaltyper                                            
+    FROM {{ source('pubs', 'TitleAuthor') }}
+),
+stg_sales AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['ord_num', 'title_id', 'stor_id']) }} AS order_number, 
+        ord_num,
+        ord_date,
+        to_char(ord_date, 'YYYYMMDD') AS orderdate_key,
+        year(ord_date) AS order_year, -- Extract year from order date
+        title_id,
+        stor_id,
+        qty
+    FROM {{ source('pubs', 'Sales') }}
 )
-
 SELECT 
     dt.titles_key,
     a.authors_key,
-    p.publishers_key,
-    dt.title_id,
-    a.author_id,
-    p.publisher_id,
-    dt.title,
-    p.publisher_name,
-    dt.published_year,
-    a.author_name,
-    dt.title_ytd_sales,
-    dt.title_royalty,
-    (dt.title_ytd_sales * dt.title_royalty / 100) AS royalty_amount_per_title,
+    s.order_year, 
+    SUM(s.qty) AS quantity_sold_per_year, 
+    SUM(s.qty * dt.title_price) AS total_sales, 
     t.royaltyper,
-    (dt.title_ytd_sales * t.royaltyper / 100) AS royalty_amount_per_author,
-    FROM stg_TitleAuthors t
+    (SUM(s.qty * dt.title_price) * t.royaltyper / 100) AS royalty_per_author, 
+     dt.title_royalty,
+    (SUM(s.qty * dt.title_price) * dt.title_royalty / 100) AS royalty_per_title 
+FROM stg_TitleAuthors t
 LEFT JOIN dim_titles dt ON t.titles_key = dt.titles_key
 LEFT JOIN dim_authors a ON t.authors_key = a.authors_key
-LEFT JOIN dim_publishers p ON dt.publishers_key = p.publishers_key
-left join dim_date d on dt.published_year= d.YEAR
+LEFT JOIN stg_sales s ON dt.title_id = s.title_id
+GROUP BY 
+    dt.titles_key,
+    a.authors_key,
+    dt.title_royalty,
+    s.order_year,
+    t.royaltyper
--- pubs/models/obt_royal_disbursement.sql
+++ pubs/models/obt_royal_disbursement.sql
@@ -2,13 +2,13 @@ WITH f_royal_disbursement AS (
     SELECT * FROM {{ ref("fact_royalty_disbursement") }}
 ),
 d_title AS (
- SELECT * EXCLUDE (publishers_key, publisher_id)
+ SELECT * 
  FROM {{ ref("dim_titles") }}
 ),
 d_author AS (
     SELECT * FROM {{ ref("dim_authors") }}
 ),
-d_publisher AS (
+d_publishers as ( 
     SELECT * FROM {{ ref("dim_publishers") }}
 ),
 d_date 
@@ -17,11 +17,11 @@ AS ( SELECT * FROM {{ ref("dim_date") }}
 SELECT 
     d_title.*, 
     d_author.*, 
-    d_publisher.*, 
-    f.royalty_amount_per_author, 
-    f.royalty_amount_per_title
+    f.quantity_sold_per_year,
+    f.total_sales,
+    f.royalty_per_author,
+    f.royalty_per_title,
+    f.order_year
 FROM f_royal_disbursement AS f
 LEFT JOIN d_title ON f.titles_key = d_title.titles_key
 LEFT JOIN d_author ON f.authors_key = d_author.authors_key
-LEFT JOIN d_publisher ON f.publishers_key = d_publisher.publishers_key
-LEFT JOIN d_date ON d_title.published_year = d_date.YEAR
