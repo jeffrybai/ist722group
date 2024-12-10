1b1330c907ccb817da04723103110669bb974500
Jeffry Bai <jbai06@g.syr.edu>
2024-12-10 04:16:42 +0000

    royalty schedule

--- pubs/models/dim_employees.sql
+++ pubs/models/dim_employees.sql
@@ -4,8 +4,6 @@ select
     FNAME as employee_firstname,
     MINIT as employee_middle_initial,
     LNAME as employee_lastname,
-    to_char(HIRE_DATE,'YYYYMMDD') as HIRE_DATE_KEY,
-    {{ dbt_utils.generate_surrogate_key(['JOB_ID']) }} as job_key,
-    {{ dbt_utils.generate_surrogate_key([pad_publisher_key('PUB_ID')]) }} as publisher_key
+    to_char(HIRE_DATE,'YYYYMMDD') as HIRE_DATE_KEY
 from 
     {{ source('pubs', 'Employee') }}
