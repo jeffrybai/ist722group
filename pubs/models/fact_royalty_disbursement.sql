0a29a9e79a304b1414b2bf965a653d7279e25ff8
Maybel Herrera <maherrer@g.syr.edu>
2024-12-11 02:43:15 +0000

    fact royalty table updated and yml

--- pubs/models/fact_royalty_disbursement.sql
+++ pubs/models/fact_royalty_disbursement.sql
@@ -1,25 +1,50 @@
-WITH dim_titles AS (
-    SELECT *
-    FROM {{ ref('dim_titles') }}
+WITH 
+stg_titles AS (
+    SELECT 
+        {{ dbt_utils.generate_surrogate_key(['title_id']) }} AS titles_key,
+        title_id,
+        title,
+        pub_id,
+        price ,
+        ytd_sales,
+        royalty 
+    FROM {{ source('pubs', 'Titles') }}
 ),
-dim_authors AS (
-    SELECT *
-    FROM {{ ref('dim_authors') }}
+stg_authors AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['AU_ID']) }} AS authors_key,    
+        AU_ID AS author_id,                                            
+        CONCAT(au_fname, ' ', au_lname) AS author_name,                                   
+        phone AS author_phone,                                            
+        address AS author_address,                                        
+        city AS author_city,                                              
+        state AS author_state,                                            
+        zip AS author_zip                                            
+    FROM {{ source('pubs', 'Authors') }}
 ),
-dim_publishers AS (
-    SELECT *
-    FROM {{ ref('dim_publishers') }}
+stg_publishers AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['pub_id']) }} AS publishers_key,
+        pub_id,
+        pub_name AS publisher_name
+    FROM {{ source('pubs', 'Publishers') }}
 ),
-dim_date AS (
-    SELECT *
-    FROM {{ ref('dim_date') }}
+
+stg_date AS (
+    SELECT 
+        ord_date,
+        TO_CHAR(ord_date, 'YYYYMMDD') AS orderdate_key,
+        EXTRACT(YEAR FROM ord_date) AS order_year
+    FROM {{ source('pubs', 'Sales') }}
 ),
 stg_TitleAuthors AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(['au_id']) }} AS authors_key, 
         {{ dbt_utils.generate_surrogate_key(['title_id']) }} AS titles_key, 
+        title_id,
+        au_id,
         AU_ORD, 
-        royaltyper                                            
+        royaltyper                               
     FROM {{ source('pubs', 'TitleAuthor') }}
 ),
 stg_sales AS (
@@ -27,30 +52,32 @@ stg_sales AS (
         {{ dbt_utils.generate_surrogate_key(['ord_num', 'title_id', 'stor_id']) }} AS order_number, 
         ord_num,
         ord_date,
-        to_char(ord_date, 'YYYYMMDD') AS orderdate_key,
-        year(ord_date) AS order_year, -- Extract year from order date
+        TO_CHAR(ord_date, 'YYYYMMDD') AS orderdate_key,
+        EXTRACT(YEAR FROM ord_date) AS order_year,
         title_id,
         stor_id,
         qty
     FROM {{ source('pubs', 'Sales') }}
 )
 SELECT 
-    dt.titles_key,
+    t.titles_key,
     a.authors_key,
-    s.order_year, 
-    SUM(s.qty) AS quantity_sold_per_year, 
-    SUM(s.qty * dt.title_price) AS total_sales, 
-    t.royaltyper,
-    (SUM(s.qty * dt.title_price) * t.royaltyper / 100) AS royalty_per_author, 
-     dt.title_royalty,
-    (SUM(s.qty * dt.title_price) * dt.title_royalty / 100) AS royalty_per_title 
-FROM stg_TitleAuthors t
-LEFT JOIN dim_titles dt ON t.titles_key = dt.titles_key
-LEFT JOIN dim_authors a ON t.authors_key = a.authors_key
-LEFT JOIN stg_sales s ON dt.title_id = s.title_id
-GROUP BY 
-    dt.titles_key,
-    a.authors_key,
-    dt.title_royalty,
-    s.order_year,
-    t.royaltyper
+    publishers_key,
+    s.order_year AS YEAR,
+    t.ytd_sales,
+    t.royalty AS Percentage_royalty,
+    (t.ytd_sales * t.royalty / 100) AS total_royalty,
+    ta.royaltyper AS AUTHORS_PERCENTAGE,
+    (t.ytd_sales * t.royalty / 100 * ta.royaltyper / 100) AS royalty_per_author
+FROM 
+    stg_TitleAuthors ta
+LEFT JOIN 
+    stg_titles t ON ta.titles_key = t.titles_key
+LEFT JOIN 
+    stg_authors a ON ta.authors_key = a.authors_key
+LEFT JOIN
+    stg_sales s ON t.title_id = s.title_id
+LEFT JOIN 
+    stg_publishers p ON t.pub_id = p.pub_id 
+ORDER BY 
+    s.order_year
