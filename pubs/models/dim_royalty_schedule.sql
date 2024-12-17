b070230c950865c56094217dfa426b86656e236b
Maybel Herrera <maherrer@g.syr.edu>
2024-12-17 01:16:05 +0000

    deleted royalsched

--- pubs/models/dim_royalty_schedule.sql
+++
@@ -1,7 +0,0 @@
-with stg_roysched as (
-    select * from {{ source('pubs', 'RoySched') }}
-)
-
-select *,
-    {{ dbt_utils.generate_surrogate_key(['title_id']) }} as titles_key,                                                   
-from stg_roysched
